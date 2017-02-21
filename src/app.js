187b05286e7494dd0dd18bfb4542a02453f81c8c
nandyjaga <nandy.jaga@gmail.com>
2017-02-21 22:55:21 +0530

    Routing and creating controllers along with use of services

--- src/app.js
+++ src/app.js
@@ -11,13 +11,87 @@ MyApp.config(function($routeProvider,$locationProvider){
     $locationProvider.hashPrefix('');
 	
 	$routeProvider
-    .when('/', {
-        templateUrl: 'view/inbox.html',
-		controller: 'InboxCtrl',
-        controllerAs: 'InboxCtrl'
-    })
-	.otherwise({
-        redirectTo: '/'
-    })
-	
+		.when('/inbox', {
+			templateUrl: 'view/inbox.html',
+			controller: 'InboxCtrl',
+			controllerAs: 'InboxCtrl'
+		})
+		.when('/sent', {
+			templateUrl: 'view/sent.html',
+			controller: 'SentCtrl',
+			controllerAs: 'SentCtrl'
+		})
+		.when('/trash', {
+			templateUrl: 'view/trash.html',
+			controller: 'TrashCtrl',
+			controllerAs: 'TrashCtrl'
+		})
+		.otherwise({
+			redirectTo: '/inbox'
+		})
 });
+
+MyApp.controller("InboxCtrl",function(sampleService){
+    var InboxCtrl = this;
+	debugger;
+	InboxCtrl.modal={
+	   appName:sampleService.appName,
+	   emails:[{
+				 subject:"Inbox Message 1"
+			   },{
+				 subject:"Inbox Message 2"
+			   },{
+				 subject:"Inbox Message 3"
+			   }]
+	}
+	
+	InboxCtrl.updateAppName=function(){
+		sampleService.updateName(InboxCtrl.modal.appName)
+	}
+});
+
+MyApp.controller("SentCtrl",function(sampleService){
+    var SentCtrl = this;
+	SentCtrl.modal={
+	   appName:sampleService.appName,
+	   emails:[{
+				 subject:"Sent Message 1"
+			   },{
+				 subject:"Sent Message 2"
+			   },{
+				 subject:"Sent Message 3"
+			   }]
+	}
+	
+	SentCtrl.updateAppName=function(){
+		sampleService.updateName(SentCtrl.modal.appName)
+	}
+});
+
+
+MyApp.controller("TrashCtrl",function(){
+    var TrashCtrl = this;
+	TrashCtrl.modal={
+	   emails:[{
+				 subject:"Trash Message 1"
+			   },{
+				 subject:"Trash Message 2"
+			   },{
+				 subject:"Trash Message 3"
+			   }]
+	}
+});
+
+
+MyApp.service("sampleService",function(){
+    this.appName="Email Client";
+	this.updateName=function(val){
+		this.appName=val;
+	}
+});
+
+
+
+
+
+
