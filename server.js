52ef0aa625ec41722e18f0a86a9599fd35cd861f
nandyjaga <nandy.jaga@gmail.com>
2017-03-01 22:57:23 +0530

    Added Mongodb

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var dbOperation = null;
+
+
+MongoClient.connect('mongodb://localhost:27017/todo', function (err, db) {
+  if (err) throw err
+	   dbOperation = db;
+	   
+})
 
 var app = express();
 app.use('/app', express.static('src'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+});
+
+app.get('/todo', function (req, res) {
+	dbOperation.collection('todo').find().toArray(function (err, result) {
+    if (err) throw err
+		res.send(result);
+    })
 });
 
 app.get('/name', function (req, res) {
